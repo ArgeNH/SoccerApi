4ad6dc8955ad139d8ed22b01457122a202c87a2e
Arge Niño <arge.nino@uptc.edu.co>
2022-04-08 18:33:07 -0500

    update docs for team

--- src/app.js
+++ src/app.js
@@ -4,30 +4,7 @@ const path = require('path');
 require('dotenv').config();
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
-
-const swg = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Soccer API',
-            version: '1.0.0',
-            description: 'Test API for Soccer',
-            contact: {
-                name: 'Arge Niño',
-                url: 'https://.argenh.github.io/',
-                email: 'darley1054@gmail.com'
-            },
-            license: {
-                name: 'MIT',
-                url: 'https://opensource.org/licenses/MIT'
-            }
-        },
-        servers: [{
-            url: 'https://soccer-api-arge.herokuapp.com/api/'
-        }]
-    },
-    apis: [`${path.join(__dirname, './routes/*.js')}`]
-};
+const swg = require('./helper/swg');
 
 const app = express();
 
--- src/routes/team.js
+++ src/routes/team.js
@@ -22,12 +22,6 @@ const router = Router();
 *    responses:
 *      200:
 *        description: Teams retrieved successfully
-*        content:
-*          application/json:
-*            schema:
-*              type: array
-*              items:
-*                $ref: "#/components/schemas/Team"
 *      400:
 *        description: Bad request
 *      500:
@@ -35,34 +29,31 @@ const router = Router();
  * 
  */
 router.get('/teams', getTeams);
-router.get('/:id', getTeam);
+
 /**
  * @swagger
- * components:
- *  schemas:
- *      Team:
- *          type: object
- *          properties:
- *              team_id:
- *                  type: string
- *                  description: id unique for each team
- *              name:
- *                  type: string
- *                  description: name of the team
- *              league:
- *                  type: string
- *                  description: league of the team
- *              country:
- *                  type: string
- *                  description: country of the team
- *          required:
- *              - team_id
- *          example:
- *              team_id: 101
- *              name: Liverpool
- *              league: Premier League
- *              country: England
+* /team/{id}:
+*   get:
+*     tags:
+*       - Team
+*     summary: Get one team
+*     description: Get all information about one team
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*           required: true
+*           description: Get team_id
+*     responses:
+*       200:
+*         description: Team retrieved successfully
+*       404:
+*         description: Team not found
+*       500:
+*         description: Internal server error
  */
+router.get('/:id', getTeam);
 
 /**
  * @swagger
@@ -93,7 +84,90 @@ router.get('/:id', getTeam);
  * 
  */
 router.post('/new', createTeam);
+
+/**
+ * @swagger
+* /team/update/{id}:
+*   put:
+*     tags:
+*       - Team
+*     summary: Update a team
+*     description: Update the information of a team
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             $ref: '#/components/schemas/Team'
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*           required: true
+*           description: Get team_id
+*     responses:
+*       200:
+*         description: Team updated
+*       404:
+*         description: Team not found
+*       500:
+*         description: Internal server error
+ */
 router.put('/update/:id', updateTeam);
+
+/**
+ * @swagger
+* /team/delete/{id}:
+*   delete:
+*     tags:
+*       - Team
+*     summary: Delete a team
+*     description: Delete the information of a team
+*     parameters:
+*       - in: path
+*         name: id
+*         schema:
+*           type: string
+*           required: true
+*           description: Get team_id
+*     responses:
+*       200:
+*         description: Team deleted
+*       404:
+*         description: Team not found
+*       500:
+*         description: Internal server error
+ */
 router.delete('/delete/:id', deleteTeam);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Team:
+ *          type: object
+ *          properties:
+ *              team_id:
+ *                  type: string
+ *                  description: id unique for each team
+ *              name:
+ *                  type: string
+ *                  description: name of the team
+ *              league:
+ *                  type: string
+ *                  description: league of the team
+ *              country:
+ *                  type: string
+ *                  description: country of the team
+ *          required:
+ *              - team_id
+ *          example:
+ *              team_id: 108
+ *              name: Everton
+ *              league: Premier League
+ *              country: Inglaterra
+ */
