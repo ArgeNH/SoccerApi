19a38c41487f2f842a205cbbb09066d1ff407818
Arge Niño <arge.nino@uptc.edu.co>
2022-04-07 23:24:52 -0500

    add mysql database in clevercloud

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'soccer_db'
+    host: process.env.DB,
+    user: 'uxzb6kfhese2dzpm',
+    password: process.env.PASSWORD_DB,
+    database: 'bhughk2pbz6e6myjf9ur'
 });
+
+//mysql://uxzb6kfhese2dzpm:0nm340LIzaPH03ryvnV2@bhughk2pbz6e6myjf9ur-mysql.services.clever-cloud.com:3306/bhughk2pbz6e6myjf9ur
 
 connection.connect(error => {
     if (error) throw new Error(`Error connecting to database: ${error}`);
